4b74b0f6fd52a4e136a5b569c578ac758ec6997b
Annie Liao <liaowow@gmail.com>
2021-10-11 19:58:30 -0400

    pass props in Tablr file

--- tablr/src/Tablr.js
+++ tablr/src/Tablr.js
@@ -1,5 +1,16 @@
 
 
-export const Tablr = () => {
+export const Tablr = ({
+  rows,
+  headers,
+  borderWidth = 'medium',
+  cellPadding = 'medium',
+  cellTextColor = 'black',
+  headerTextColor = 'white',
+  cellBorderColor = '#747381',
+  headerBorderColor = '#494866',
+  backgroundColor = '#a6a5b8',
+  headerBackgroundColor = '#65647a',
+}) => {
   return (<>TABLR</>)
 }
